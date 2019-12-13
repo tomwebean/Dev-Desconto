e8d27f80a000ea1b935cf2a2989ba775fb9a9e7f
Wellington Bernardes <webean@outlook.com>
2019-12-13 02:44:57 -0200

    Imprimindo um valor - Fim

--- aula001.js
+++ aula001.js
@@ -1,4 +1,8 @@
 // Declarando uma variável
-var preco = 54.90;
+var preço = 54.90;
+
 // Multiplicando um número
-var desconto = preço * 0,1;
+var desconto = preço * 0.1;
+
+// Imprimindo um valor
+console.log("O valor do desconto é " + desconto);
